57ce8132908c1b37dbaf5f2c7bc8d57df9199c2f
Thomas Museus Dabay <tmuseusdabay@pm.me>
2020-03-04 15:19:11 -0600

    Updates

---
+++ precache-manifest.f3fe4d109b40a79c6252f030fe4a3203.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95dd0edbfd951208d700a87093a93c10",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "ddef5e97f0ee5a8de56f",
+    "url": "/pomodoro-clock/static/css/main.495a13f3.chunk.css"
+  },
+  {
+    "revision": "8a6ffdc22051a342a5f6",
+    "url": "/pomodoro-clock/static/js/2.c2044783.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/pomodoro-clock/static/js/2.c2044783.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddef5e97f0ee5a8de56f",
+    "url": "/pomodoro-clock/static/js/main.191905bc.chunk.js"
+  },
+  {
+    "revision": "da1ac70f2cc3523843a7",
+    "url": "/pomodoro-clock/static/js/runtime-main.8c4bee0e.js"
+  }
+]);
