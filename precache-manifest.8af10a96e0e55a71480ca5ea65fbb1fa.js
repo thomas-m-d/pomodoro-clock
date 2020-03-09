c4da9dfd16668df24edcb1cb4847711bc6235896
Thomas Museus Dabay <tmuseusdabay@pm.me>
2020-03-09 17:50:55 -0500

    Updates

---
+++ precache-manifest.8af10a96e0e55a71480ca5ea65fbb1fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "030ef9399a8ab552be1b0ab65026cd47",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "0146a829330ae8dd0119",
+    "url": "/pomodoro-clock/static/css/main.0adc8d4a.chunk.css"
+  },
+  {
+    "revision": "b275bacd6f424ab4d983",
+    "url": "/pomodoro-clock/static/js/2.e3f5d947.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/pomodoro-clock/static/js/2.e3f5d947.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0146a829330ae8dd0119",
+    "url": "/pomodoro-clock/static/js/main.a6449ab8.chunk.js"
+  },
+  {
+    "revision": "da1ac70f2cc3523843a7",
+    "url": "/pomodoro-clock/static/js/runtime-main.8c4bee0e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pomodoro-clock/precache-manifest.f3fe4d109b40a79c6252f030fe4a3203.js"
+  "/pomodoro-clock/precache-manifest.8af10a96e0e55a71480ca5ea65fbb1fa.js"
 );
 
 self.addEventListener('message', (event) => {
